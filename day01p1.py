1d489f2cdbc629088b007d3ee78b76e2cb5c4ac6
ayleph <ayleph@thisshitistemp.com>
2021-12-10 10:51:28 -0800

    Include destructive and non-destructive approaches

--- day01p1.py
+++ day01p1.py
@@ -10,10 +10,19 @@ args = parser.parse_args()
 with open(args.file, "r") as f:
     depths = f.read().splitlines()
 
+# non-destructive method
+n = 0
+increaseCount = 0
+while n < len(depths) - 1:
+    if (depths[n + 1] > depths[n]):
+        increaseCount = increaseCount + 1
+    n = n + 1
+print(increaseCount)
+
+# quick and dirty destructive method
 increaseCount = 0
 while len(depths) > 1:
     if (depths[1] > depths[0]):
         increaseCount = increaseCount + 1
     depths.pop(0)
-
 print(increaseCount)
--- day01p2.py
+++ day01p2.py
@@ -10,18 +10,30 @@ args = parser.parse_args()
 with open(args.file, "r") as f:
     depths = f.read().splitlines()
 
+# non-destructive method
 n = 0
+depthSums = []
+while n < len(depths) - 2:
+    depthSums.append(int(depths[n]) + int(depths[n + 1]) + int(depths[n + 2]))
+    n = n + 1
+
+n = 0
+increaseCount = 0
+while n < len(depthSums) - 1:
+    if (depthSums[n + 1] > depthSums[n]):
+        increaseCount = increaseCount + 1
+    n = n + 1
+print(increaseCount)
+
+# quick and dirty destructive method
 depthSums = []
 while len(depths) > 2:
     depthSums.append(int(depths[0]) + int(depths[1]) + int(depths[2]))
-    n = n + 1
     depths.pop(0)
 
 increaseCount = 0
 while len(depthSums) > 1:
     if (depthSums[1] > depthSums[0]):
         increaseCount = increaseCount + 1
-    #print(depths[0], depths[1], increaseCount)
     depthSums.pop(0)
-
 print(increaseCount)
